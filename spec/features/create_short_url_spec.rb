80034e4eb777c9e152c2f3c704cb97c85e2411d2
Nathan R <nathan19@hotmail.com>
2015-04-08 22:27:09 -0700

    added some tests to create short url

--- spec/features/create_short_url_spec.rb
+++ spec/features/create_short_url_spec.rb
@@ -2,6 +2,26 @@ require 'spec_helper'
 
 describe "creating a short url" do
   context "when not signed in" do
+
+    it "lets me get a new short url" do 
+      visit dashboard_path 
+
+      fill_in "link_long_url", with: "http://cnn.com"
+      click_button "Shorten It!"
+      expect(page).to have_content "http://cnn.com"
+      expect(page).to have_content "links to"
+
+    end 
+
+    it "does not let me enter an invalid url" do 
+      visit dashboard_path 
+
+      fill_in "link_long_url", with: "hbiob"
+      click_button "Shorten It!"
+
+      expect(page).to have_content "MAKE YOUR LINKS COUNT!"
+    end 
+
   end
 
   context "when signed in" do
@@ -10,5 +30,25 @@ describe "creating a short url" do
     before do
       login_as user
     end
+
+    it "lets me get a new short url" do 
+      visit dashboard_path 
+
+      fill_in "link_long_url", with: "http://cnn.com"
+      click_button "Shorten It!"
+
+      expect(page).to have_content "links to"
+      expect(page).to have_content "http://cnn.com"
+    end 
+
+    it "does not let me enter an invalid url" do 
+      visit dashboard_path
+
+      fill_in "link_long_url", with: "hbiob"
+      click_button "Shorten It!"
+
+      expect(page).to have_content "MAKE YOUR LINKS COUNT!"
+    end 
+
   end
 end
