163903472da7d34a7ab295f662dae2e43391069e
Nathan R <nathan19@hotmail.com>
2015-04-22 21:27:10 -0700

    added test for mailer

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -16,4 +16,18 @@ describe UserMailer, :type => :mailer do
       expect(mail.html_part.body).to include "You have successfully"
     end
   end
+
+  describe "#settings_email" do
+    let(:mail) { UserMailer.settings_email(user) }
+
+    it "renders the correct headers" do
+      expect(mail.from).to include "mail@benwoodall.com"
+      expect(mail.to).to include user.email
+      expect(mail.subject).to include "Your Shortener settings have changed"
+    end
+
+    it "renders an html layout" do
+      expect(mail.html_part.body).to include "Your settings have been updated"
+    end
+  end
 end
